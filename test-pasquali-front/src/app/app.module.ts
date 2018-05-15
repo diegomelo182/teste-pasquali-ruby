848622b70d7debdcf932e711889a910252191ad5
Diego Melo <diegomelo182@gmail.com>
2018-05-15 00:07:17 -0300

    tarefa: Construindo estrutura do front

--- test-pasquali-front/src/app/app.module.ts
+++ test-pasquali-front/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginService } from './login/login.service';
+
+const modules = [
+  BrowserModule,
+  HttpClientModule,
+  AppRoutingModule
+];
+
+const components = [
+  AppComponent,
+  LoginComponent
+];
+
+const services = [
+  LoginService
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    ...components
   ],
   imports: [
-    BrowserModule
+    ...modules
   ],
-  providers: [],
+  providers: [
+    ...services
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
